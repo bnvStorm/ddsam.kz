221438e9ad8a2ca44cf1dd620ed696adb0319fbe
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2021-03-29 12:37:25 +0600

    Update csvphp.php

--- inc/csvphp.php
+++ inc/csvphp.php
@@ -1,6 +1,6 @@
 <?php 
 // качаем категории и страницы из гугл диска и сохраняем на сервере
-//$categories = file_get_contents('https://docs.google.com/spreadsheets/d/1R87zOW2-990ATjZmc9qHu_CLqkFvLdqFJlY3TWzHgmU/export?format=csv&gid=1689854645');
+$categories = file_get_contents('https://docs.google.com/spreadsheets/d/1R87zOW2-990ATjZmc9qHu_CLqkFvLdqFJlY3TWzHgmU/export?format=csv&gid=1689854645');
 file_put_contents('categories.csv', $categories);
 
 function display_products($category,$array) {
